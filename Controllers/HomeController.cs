f3fffb362181c525bd660ab3de58327e3b981e63
Jason Horvath <Jason.Horvath1@outlook.com>
2020-08-20 10:56:43 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MetricConversion.Models;
+
+namespace MetricConversion.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult Conversion()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult Conversion(ConversionEntries entries)
+        {
+            //TODO: store entries from user
+            return View("Results", entries);
+        }
+    }
+}
---
+++ Models/ConversionEntries.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetricConversion.Models
+{
+    public class ConversionEntries
+    {
+        public double Miles { get; set; }
+
+        public double Kilometers { get; set; }
+    }
+}
